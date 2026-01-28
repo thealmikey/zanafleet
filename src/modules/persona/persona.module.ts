144e5f2a0b2aec962ee5a72f9f19cae6a172ae8b
thealmikey <thealmikey@gmail.com>
2026-01-28 09:43:13 +0300

    feat: implement persona assignment to actor command and handler

--- src/modules/persona/persona.module.ts
+++ src/modules/persona/persona.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { PersonaEntity } from './entities/persona.entity';
+import { ActorEntity } from '../actor/entities/actor.entity';
+import { WorkspaceEntity } from '../workspace/entities/workspace.entity';
 import { ActorPersonaEntity } from './entities/actor-persona.entity';
+import { PersonaEntity } from './entities/persona.entity';
+import { AssignPersonaToActorCommandHandler } from './handlers/assign-persona-to-actor.handler';
 import { CreatePersonaCommandHandler } from './handlers/create-persona.handler';
 
 @Module({
   imports: [
     CqrsModule,
-    TypeOrmModule.forFeature([PersonaEntity, ActorPersonaEntity]),
+    TypeOrmModule.forFeature([
+      PersonaEntity,
+      ActorPersonaEntity,
+      ActorEntity,
+      WorkspaceEntity,
+    ]),
   ],
-  providers: [CreatePersonaCommandHandler],
-  exports: [TypeOrmModule, CreatePersonaCommandHandler],
+  providers: [CreatePersonaCommandHandler, AssignPersonaToActorCommandHandler],
+  exports: [
+    TypeOrmModule,
+    CreatePersonaCommandHandler,
+    AssignPersonaToActorCommandHandler,
+  ],
 })
 export class PersonaModule {}
