4225984e4830707fd8471582fba1be782587191a
thealmikey <thealmikey@gmail.com>
2026-01-29 03:23:53 +0300

    feat: add signup types and enums for frontend session management

---
+++ src/frontend/types/index.ts
@@ -0,0 +1,10 @@
+export {
+  ActorType,
+  SignUpSessionStatus,
+  type InitiateSignupRequest,
+  type InitiateSignupResponse,
+  type UpdateStepRequest,
+  type UpdateStepResponse,
+  type SignupSession,
+  type FinalizeSignupResponse,
+} from './signup.types';
