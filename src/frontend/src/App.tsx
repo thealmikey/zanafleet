c5f363b59ee6648e9137a5763d58ea2e5d769633
thealmikey <thealmikey@gmail.com>
2026-02-01 19:34:43 +0300

    feat: Add React app setup with routing and basic pages

---
+++ src/frontend/src/App.tsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { ThemeProvider, createTheme, CssBaseline } from '@mui/material';
+
+import HomePage from './components/HomePage';
+import SignIn from './components/SignIn';
+import Dashboard from './components/Dashboard';
+import SignupWizard from './components/SignupWizard';
+import ProtectedRoute from './components/ProtectedRoute';
+
+const theme = createTheme();
+
+function App(): React.ReactElement {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/signin" element={<SignIn />} />
+          <Route path="/signup" element={<SignupWizard />} />
+          <Route
+            path="/dashboard"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
+}
+
+export default App;
