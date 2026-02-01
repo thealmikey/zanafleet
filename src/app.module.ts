f23a2c4ac1a03c875a4d40f3e9d8f7036e51f82c
thealmikey <thealmikey@gmail.com>
2026-02-01 17:24:09 +0300

    feat(app.module): Add TypeORM configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { EventBusModule } from './core/event-bus';
 import { Neo4jModule } from './core/neo4j';
@@ -11,6 +12,16 @@ import { OrganizationModule } from './modules/organization/organization.module';
  */
 @Module({
   imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: parseInt(process.env.DB_PORT || '5432', 10),
+      username: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || 'postgres',
+      database: process.env.DB_NAME || 'zanafleet',
+      autoLoadEntities: true,
+      synchronize: process.env.NODE_ENV !== 'production',
+    }),
     EventBusModule.forRoot({
       isGlobal: true,
     }),
