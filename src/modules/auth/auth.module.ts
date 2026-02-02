8ce602b6f30964902471d14b46df008479b45e5f
thealmikey <thealmikey@gmail.com>
2026-02-02 12:16:46 +0300

    feat: Add JWT and Keycloak authentication support
    This change introduces JWT and Keycloak authentication support to the application.
    
    - Adds configuration for JWT and Keycloak in `auth.config.ts`
    - Integrates Keycloak using the `nest-keycloak-connect` library
    - Adds support for JWT-based authentication in the `AuthModule`

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,19 +1,26 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { CqrsModule } from '@nestjs/cqrs';
 import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { ActorEntity } from '../actor/entities/actor.entity';
 
+import { authConfig, keycloakConfig } from './config/auth.config';
 import { AuthController } from './controllers/auth.controller';
 import { LoginCommandHandler } from './handlers/login.handler';
 
 /**
  * Auth Module
  *
- * Handles user authentication.
+ * Handles user authentication with JWT and Keycloak support.
  */
 @Module({
-  imports: [CqrsModule, TypeOrmModule.forFeature([ActorEntity])],
+  imports: [
+    ConfigModule.forFeature(authConfig),
+    ConfigModule.forFeature(keycloakConfig),
+    CqrsModule,
+    TypeOrmModule.forFeature([ActorEntity]),
+  ],
   controllers: [AuthController],
   providers: [LoginCommandHandler],
   exports: [],
