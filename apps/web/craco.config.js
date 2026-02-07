d190dc79ee8b4cf12bbd324024988bf41c48b0e1
thealmikey <thealmikey@gmail.com>
2026-02-07 11:47:30 +0300

    refactor: link web app to contracts source instead of dist package

--- apps/web/craco.config.js
+++ apps/web/craco.config.js
@@ -4,12 +4,29 @@ const path = require('path');
 module.exports = {
   webpack: {
     alias: {
-      '@zanafleet/contracts': path.resolve(__dirname, '../../packages/contracts/dist'),
+      '@zanafleet/contracts': path.resolve(__dirname, '../../packages/contracts/src'),
     },
     configure: (webpackConfig) => {
       webpackConfig.resolve.plugins = webpackConfig.resolve.plugins.filter(
         (plugin) => plugin.constructor.name !== 'ModuleScopePlugin'
       );
+
+      const contractsSrc = path.resolve(__dirname, '../../packages/contracts/src');
+
+      const babelLoaderRule = webpackConfig.module.rules
+        .find((rule) => rule.oneOf)
+        ?.oneOf?.find(
+          (rule) => rule.loader && rule.loader.includes('babel-loader') && rule.include
+        );
+
+      if (babelLoaderRule) {
+        if (Array.isArray(babelLoaderRule.include)) {
+          babelLoaderRule.include.push(contractsSrc);
+        } else {
+          babelLoaderRule.include = [babelLoaderRule.include, contractsSrc];
+        }
+      }
+
       return webpackConfig;
     },
   },
